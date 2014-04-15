ff8a351c4bd65ec19d82ea0bb5e6c134a9e7c42e
Li Zhenchao <leezhenchao@gmail.com>
2014-04-15 20:09:26 +0800

    bug fix, version 0.2.5

--- lib/exception_gateway/vendors/bugsnag.rb
+++ lib/exception_gateway/vendors/bugsnag.rb
@@ -2,9 +2,9 @@ module ExceptionGateway
   module Vendors
     def bugsnag_alert(msg, options={})
       if ExceptionGateway.config.bugsnag_alert_api_key
-        Bugsnag.notify(RuntimeError.new(msg), {:details => options})
-      else
         Bugsnag.notify(RuntimeError.new(msg), {:details => options}, api_key: ExceptionGateway.config.bugsnag_alert_api_key)
+      else
+        Bugsnag.notify(RuntimeError.new(msg), {:details => options})
       end
     end
 
